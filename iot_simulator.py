db7d848b383f816742c4e68722741dc950d32f5b
KitCat <103929780+KitCatJ@users.noreply.github.com>
2026-07-24 09:59:56 +0100

    changed devices_map dictionary into a json file and wrote a try/except for ctr-c

--- iot_simulator.py
+++ iot_simulator.py
@@ -1,19 +1,10 @@
 import time
 import random
 from datetime import datetime
+import json
 
-device_map = {
-    "camera_01": "192.168.1.101",
-    "thermostat_02": "192.168.1.102",
-    "smart_lock_03": "192.168.1.103",
-    "smart_lock_04": "192.168.1.104",
-    "smart_light_05": "192.168.1.105",
-    "smart_light_06": "192.168.1.106",
-    "smart_light_07": "192.168.1.107",
-    "smart_light_08": "192.168.1.108",
-    "smart_light_09": "192.168.1.109",
-    "smart_light_10": "192.168.1.110"
-}
+with open("devices.json", "r") as file:
+    device_map = json.load(file)
 
 possible_ports = [80, 443, 22, 23]
 
@@ -32,25 +23,29 @@ def inspect_packages(packet):
     else:
         print(f"Device Packet: {packet}")
 
-while True:
-    bytes_sent = random.randint(50, 1500)
-    current_device = random.choice(list(device_map.keys()))
-    current_ip = device_map[current_device]
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    current_port = random.choice(possible_ports)
+try:
+    while True:
+        bytes_sent = random.randint(50, 1500)
+        current_device = random.choice(list(device_map.keys()))
+        current_ip = device_map[current_device]
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        current_port = random.choices(possible_ports, weights=[45, 45, 5, 5])[0]
 
-    device_packet = {
-    "device_id": current_device,
-    "bytes_sent": bytes_sent,
-    "ip_address": current_ip,
-    "status": "connected",
-    "request_count": request_count,
-    "timestamp": current_time,
-    "target_port": current_port
-    }
+        device_packet = {
+        "device_id": current_device,
+        "bytes_sent": bytes_sent,
+        "ip_address": current_ip,
+        "status": "connected",
+        "request_count": request_count,
+        "timestamp": current_time,
+        "target_port": current_port
+        }
 
-    request_count += 1
+        request_count += 1
 
-    inspect_packages(device_packet)
-        
-    time.sleep(1)
+        inspect_packages(device_packet)
+            
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print(f"\n🛑 System shutting down. Total packets scanned: {request_count}")
